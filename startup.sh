c553eb9998dbbbc4f425b3aae635424b5041e7aa
ovdickson <dickson@cloudplexo.com>
2024-12-09 21:19:25 +0100

    edited

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod +x startup.sh
-python3 manage.py migrate && python manage.py collectstatic && gunicorn EBARestAPIServer.wsgi:application --bind 0.0.0.0:8000 --workers 2
+cd /var/task && exec gunicorn EBARestAPIServer.wsgi:application --bind 0.0.0.0:8000 --workers 2
