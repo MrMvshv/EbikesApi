1089455e49df73cc950c64644620b4c0ce4df06c
ovdickson <dickson@cloudplexo.com>
2024-12-09 22:00:40 +0100

    edited

--- startup.sh
+++ startup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-chmod +x startup.sh
-gunicorn EBARestAPIServer.wsgi:application --bind 0.0.0.0:8000 --workers 2
+exec gunicorn EBARestAPIServer.wsgi:application --bind 0.0.0.0:8000 --workers 2
