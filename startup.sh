e5b039782fe3168306eeb882f8095f7b42fdc560
ovdickson <dickson@cloudplexo.com>
2024-12-10 14:38:01 +0100

    edited

---
+++ startup.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+python3 manage.py migrate --no-input && python3 manage.py collectstatic --no-input && gunicorn EBARestAPIServer.wsgi:application --bind 0.0.0.0:8000 --workers 2 --access-logfile '-'  --error-logfile '-'
